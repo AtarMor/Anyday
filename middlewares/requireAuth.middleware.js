7164038afd8ba66211cab36a561fc6931bbeb449
Atar Mor <atarmor92@gmail.com>
2024-04-14 22:32:59 +0300

    Implement user & auth RESTful API

--- middlewares/requireAuth.middleware.js
+++ middlewares/requireAuth.middleware.js
@@ -7,7 +7,7 @@ export function requireAuth(req, res, next) {
   req.loggedinUser = loggedinUser
   
   if (config.isGuestMode && !loggedinUser) {
-    req.loggedinUser = { _id: '', fullname: 'Guest' }
+    req.loggedinUser = { _id: '', fullName: 'Guest' }
     return next()
   }
   if (!loggedinUser) return res.status(401).send('Not Authenticated')
@@ -18,7 +18,7 @@ export function requireAdmin(req, res, next) {
   const { loggedinUser } = asyncLocalStorage.getStore()
   if (!loggedinUser) return res.status(401).send('Not Authenticated')
   if (!loggedinUser.isAdmin) {
-    logger.warn(loggedinUser.fullname + 'attempted to perform admin action')
+    logger.warn(loggedinUser.fullName + 'attempted to perform admin action')
     res.status(403).end('Not Authorized')
     return
   }
