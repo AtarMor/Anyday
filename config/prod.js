ee206363850beb8316b2a67fba250c9a5ec4c613
Atar Mor <atarmor92@gmail.com>
2024-04-15 12:13:21 +0300

    Change db name on prod

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
     dbURL: process.env.MONGO_URL || 'mongodb+srv://atarmor92:FDby$wmDCTCu6wZ@cluster0.e3x2fqd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    dbName: process.env.DB_NAME || 'board_db'
+    dbName: process.env.DB_NAME || 'boardDB'
 }
